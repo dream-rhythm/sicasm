18c38ac0c44c705d3e1c5ac7e71cdc5fce97642a
fcu-d0441320 <d0441320@fcu.edu.tw>
2017-04-29 19:23:58 +0800

    修正errMessage編碼錯誤 新增enum類型來管理type

--- header.hpp
+++ header.hpp
@@ -13,5 +13,15 @@
 #include<cstdlib>
 using namespace std;
 
+typedef enum {
+    START=0,
+    END=1,
+    RES=2,
+    CONST=3,
+    OPERATOR=4,
+    COMMENT=5,
+    ORG=6
+}TYPE;
+
 
 #endif // HEADER
