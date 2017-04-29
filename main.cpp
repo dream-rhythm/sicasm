e99da906de5bca3b25e090bf7f0d306c66cab551
fcu-d0441320 <d0441320@fcu.edu.tw>
2017-04-29 20:09:32 +0800

    完成pass2註解

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int setFileName(int,char*[],AsmSetting&);
 int main(int arg_num,char* arg_data[]){
     AsmSetting Setting;
     stringstream ss;
-    string tmpStringData;
+    string tmpStr;
     unsigned int tmpData;
     bool stopObjFileOutput;
     bool is_end_statament=false;
@@ -53,137 +53,131 @@ int main(int arg_num,char* arg_data[]){
         }
         //Comment
         else if(tmpCode->get_type()==COMMENT);                                      //如果是註解  啥都不做
+        else if(tmpCode->get_type()==END);                                          //如果是END  啥都不做
         else if(tmpCode->get_type()==ORG){                                          //如果是ORG指令
-            if(tmpCode->get_data()==""){                                                //如果未指定ORG的位址
+            if(tmpCode->get_data()==""){                                            //如果未指定ORG的位址
                 if(LocCtr->set_org());                                                  //跳回原本的地方
                 else tmpCode->add_ErrMes(63,Setting.language);                          //跳回失敗的話        噴錯誤63(ORG statement must be used before jump back)
             }
-            else{
-                tmpStringData = tmpCode->get_data();
-                unsigned int loc=0;
-                if(tmpStringData.find("+")){
-                    if(SymbolTAB->is_in(tmpCode->get_data().substr(0,tmpStringData.find("+"))))loc = SymbolTAB->get_address(tmpCode->get_data().substr(0,tmpStringData.find("+")));
-                    else tmpCode->add_ErrMes(36,Setting.language);
-                    loc+=atoi(tmpCode->get_data().substr(tmpStringData.find("+")).c_str());
+            else{                                                                   //如果有指定ORG的位址
+                tmpStr = tmpCode->get_data();                                       //建立暫存
+                unsigned int loc=0;                                                     //設LOC=0
+                if(tmpStr.find("+")){                                                   //如果位址中找到+
+                    string label=tmpCode->get_data().substr(0,tmpStr.find("+"));            //分割出label
+                    string number = tmpCode->get_data().substr(tmpStr.find("+"));           //以及後面的數字
+                    if(SymbolTAB->is_in(label))loc = SymbolTAB->get_address(label);         //如果有找到label 設定對應的location
+                    else tmpCode->add_ErrMes(36,Setting.language);                          //否則    噴錯誤36(Symbol must be defined before used in ORG statement)
+                    loc+=atoi(number.c_str());                                              //加上最後的數字
                 }
-                else if(tmpStringData.find("-")){
-                    if(SymbolTAB->is_in(tmpCode->get_data().substr(0,tmpStringData.find("-"))))loc = SymbolTAB->get_address(tmpCode->get_data().substr(0,tmpStringData.find("+")));
-                    else tmpCode->add_ErrMes(36,Setting.language);
-                    loc+=atoi(tmpCode->get_data().substr(tmpStringData.find("-")).c_str());
+                else if(tmpStr.find("-")){                                              //如果位址中找到-
+                    string label=tmpCode->get_data().substr(0,tmpStr.find("-"));            //分割出label
+                    string number = tmpCode->get_data().substr(tmpStr.find("-"));           //以及後面的數字
+                    if(SymbolTAB->is_in(label))loc = SymbolTAB->get_address(label);         //如果有找到label 設定對應的location
+                    else tmpCode->add_ErrMes(36,Setting.language);                          //否則    噴錯誤36(Symbol must be defined before used in ORG statement)
+                    loc-=atoi(number.c_str());                                              //減去最後的數字
                 }
-                else{
-                    if(SymbolTAB->is_in(tmpCode->get_data()))loc = SymbolTAB->get_address(tmpCode->get_data());
-                    else tmpCode->add_ErrMes(36,Setting.language);
+                else{                                                                   //如果沒有+-
+                    if(SymbolTAB->is_in(tmpCode->get_data()))loc = SymbolTAB->get_address(tmpCode->get_data());//如果有找到label 設定對應的location
+                    else tmpCode->add_ErrMes(36,Setting.language);                          //否則    噴錯誤36(Symbol must be defined before used in ORG statement)
                 }
-                LocCtr->set_org(loc);
+                LocCtr->set_org(loc);                                                   //設定LocationCounter為指定的目標
             }
         }
-        else{
-            if(tmpCode->get_label()!=""){
-                tmpStringData = tmpCode->get_label();
-                bool isok=true;
-                for(unsigned int j=0;j<tmpStringData.length();j++)if(!isalnum(tmpStringData.at(j)))isok=false;
-                if(!isok)tmpCode->add_ErrMes(1,Setting.language);
-                if( SymbolTAB->is_in(tmpStringData) ){
-                    tmpCode->add_ErrMes(26,Setting.language);
-                    SymbolTAB->setErrflag(tmpCode->get_label());
+        else{                                                                       //其他(operator,word,byte,resw,resb)
+            if(tmpCode->get_label()!=""){                                           //如果label不為空
+                tmpStr = tmpCode->get_label();                                      //存入暫存用變數
+                bool isok=true;                                                     //設檢查器為true
+                for(unsigned j=0;j<tmpStr.length();j++)if(!isalnum(tmpStr.at(j)))isok=false;//如果有不合規定的字 設檢查器為false
+                if(!isok)tmpCode->add_ErrMes(1,Setting.language);                   //如果檢查失敗     噴錯誤1(Illegal format in label field)
+                if( SymbolTAB->is_in(tmpStr) ){                                     //如果這個label已經存在
+                    tmpCode->add_ErrMes(26,Setting.language);                       //噴錯誤26(Duplicate label definition)
+                    SymbolTAB->setErrflag(tmpCode->get_label());                    //並把這個label設成錯誤標籤
                 }
-                else if(Opcode::is_in(tmpCode->get_label()))tmpCode->add_ErrMes(1,Setting.language);
-                else SymbolTAB->append(tmpCode->get_label(),LocCtr->get_nowLoc());
+                else if(Opcode::is_in(tmpStr))tmpCode->add_ErrMes(1,Setting.language);//如果這個label是operator 噴錯誤1(Illegal format in label field)
+                else SymbolTAB->append(tmpStr,LocCtr->get_nowLoc());                 //否則 加入symbol存起來
             }
-            if(Opcode::is_in(tmpCode->get_opcode()))LocCtr->add(Opcode::get_length(tmpCode->get_opcode(),"SIC"));
-            else if(tmpCode->get_type()==2||tmpCode->get_type()==3)LocCtr->add(tmpCode->get_opcode(),tmpCode->get_data());//WORD BYTE RESW RESB
-            else if(tmpCode->get_type()==1);    //END
-            else tmpCode->add_ErrMes(2,Setting.language);
+            tmpStr=tmpCode->get_opcode();                                           //獲取指令
+            if(Opcode::is_in(tmpStr))LocCtr->add(Opcode::get_length(tmpStr,"SIC")); //如果他是opcode 把LocationCounter加上對應的空間
+            else if(tmpCode->get_type()==RES||tmpCode->get_type()==CONST)LocCtr->add(tmpStr,tmpCode->get_data());//WORD BYTE RESW RESB
+            else tmpCode->add_ErrMes(2,Setting.language);                           //如果都不是   噴錯誤2(Illegal format in operation field)
         }
     }
 
 
     //Pass2
-    for(int i=1;i<=AsmTAB->get_lines();i++){
-        tmpCode = AsmTAB->findLine(i);
-        if(is_end_statament&&tmpCode->get_type()!=5)tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(6,Setting.language));
-        if(tmpCode->get_type()==0);         //START
-        else if(tmpCode->get_type()==1){    //END
-            if(tmpCode->get_data()=="");
-            else if(SymbolTAB->is_in(tmpCode->get_data()));
-            else tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(2,Setting.language));
-            is_end_statament =true;
+    for(int i=1;i<=AsmTAB->get_lines();i++){                                       //從第一行開始
+        tmpCode = AsmTAB->findLine(i);                                             //指定該行
+        if(is_end_statament&&tmpCode->get_type()!=COMMENT)tmpCode->add_ErrMes(6,Setting.language);//如果讀到END語句 後面還有非註解的東西 噴錯誤6(Undefined label after END statement)
+        if(tmpCode->get_type()==START);                                            //如果是START語句   啥都不做
+        else if(tmpCode->get_type()==END){                                         //如果是END語句
+            if(tmpCode->get_data()=="");                                                //如果沒有指定位址     啥都不做
+            else if(SymbolTAB->is_in(tmpCode->get_data()));                             //如果找的到指定的位址 也是啥都不做
+            else tmpCode->add_ErrMes(2,Setting.language);                               //如果找不到      噴錯誤2(Illegal format in operation field)
+            is_end_statament =true;                                                     //把end旗標設成true
         }
-        else if(tmpCode->get_type()==2);    //RESW RESB
-        else if(tmpCode->get_type()==3){    //WORD BYTE
-            if(tmpCode->get_opcode()=="WORD"){
-                ss.clear();
-                ss.str("");
-                ss<<tmpCode->get_data();
-                ss>>tmpData;
-                if(!ss.fail()&&tmpData>=0&&tmpData<=16777215)tmpCode->set_objcode(tmpData);
-                else tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(9,Setting.language));
+        else if(tmpCode->get_type()==RES);                                          //RESW RESB    啥都不做
+        else if(tmpCode->get_type()==CONST){                                        //WORD BYTE
+            if(tmpCode->get_opcode()=="WORD"){                                          //如果是word
+                ss.clear();                                                             //清空轉換器旗標
+                ss.str("");                                                             //清空轉換器內容
+                ss<<tmpCode->get_data();                                                //輸入
+                ss>>tmpData;                                                            //輸出
+                if(!ss.fail()&&tmpData>=0&&tmpData<=0xffffff)tmpCode->set_objcode(tmpData); //如果輸出成功 就設定objcode
+                else tmpCode->add_ErrMes(9,Setting.language);                               //轉換失敗 噴錯誤9(Illegal hex string in BYTE statement)
             }
-            else if(tmpCode->get_opcode()=="BYTE"){
-                if(tmpCode->get_data().at(0)=='X'||tmpCode->get_data().at(0)=='x'){
-                    bool errFlag=false;
-                    if(tmpCode->get_data().length()>3){
-                        if(tmpCode->get_data().at(1)!='\'')errFlag=true;
-                        if(tmpCode->get_data().at(tmpCode->get_data().length()-1)!='\'')errFlag=true;
-                    }
-                    else errFlag=true;
-                    ss.clear();
-                    ss.str("");
-                    for(unsigned int j=2;j<tmpCode->get_data().length()-1;j++){
-                        if(isxdigit(tmpCode->get_data().at(j)))
-                            ss<<(char)tolower(tmpCode->get_data().at(j));
-                    else errFlag=true;
-                    }
-                    ss>>tmpStringData;
-                    if(errFlag==false)tmpCode->set_objcode(tmpStringData);
-                    else tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(9,Setting.language));
+            else if(tmpCode->get_opcode()=="BYTE"){                                     //如果是byte
+                bool errFlag=false;                                                         //設錯誤旗標為false
+                if(tmpCode->get_data().length()>3){                                         //若長度>3
+                    if(tmpCode->get_data().at(1)!='\'')errFlag=true;                        //檢查有沒有被單引號刮起來
+                    if(tmpCode->get_data().at(tmpCode->get_data().length()-1)!='\'')errFlag=true;   //若缺少其中一邊 把錯誤旗標設成true
                 }
-                else if(tmpCode->get_data().at(0)=='C'||tmpCode->get_data().at(0)=='c'){
-                    bool errFlag=false;
-                    if(tmpCode->get_data().length()>3){
-                        if(tmpCode->get_data().at(1)!='\'')errFlag=true;
-                        if(tmpCode->get_data().at(tmpCode->get_data().length()-1)!='\'')errFlag=true;
+                else errFlag=true;                                                          //若長度不足3直接設成錯誤
+                ss.clear();                                                                 //清空轉換器旗標
+                ss.str("");                                                                 //清空轉換器內容
+                if(tmpCode->get_data().at(0)=='X'||tmpCode->get_data().at(0)=='x'){         //如果是x
+                    for(unsigned j=2;j<tmpCode->get_data().length()-1;j++){                      //一個字一個字
+                        if(isxdigit(tmpCode->get_data().at(j)))                                      //如果是合法的字
+                            ss<<(char)tolower(tmpCode->get_data().at(j));                            //塞到轉換器
+                        else errFlag=true;                                                           //不合法就設錯誤旗標為true
                     }
-                    else errFlag=true;
-                    ss.clear();
-                    ss.str("");
-                    for(unsigned int j=2;j<tmpCode->get_data().length()-1;j++)ss<<std::hex<<(unsigned int)tmpCode->get_data().at(j)<<std::dec;
-                    ss>>tmpStringData;
-                    if(errFlag==false) tmpCode->set_objcode(tmpStringData);
-                    else tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(9,Setting.language));
                 }
-                else tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(9,Setting.language));
+                else if(tmpCode->get_data().at(0)=='C'||tmpCode->get_data().at(0)=='c'){     //如果是c
+                    for(unsigned j=2;j<tmpCode->get_data().length()-1;j++)                      //一個字一個字
+                        ss<<std::hex<<(unsigned int)tmpCode->get_data().at(j)<<std::dec;            //塞到轉換器
+                }
+                else tmpCode->add_ErrMes(9,Setting.language);                                //如果不是x,c 噴錯誤9(Illegal hex string in BYTE statement)
+                ss>>tmpStr;                                                                  //輸出
+                if(errFlag==false)tmpCode->set_objcode(tmpStr);                              //如果通過檢驗 設定objcode
+                else tmpCode->add_ErrMes(9,Setting.language);                                //否則 噴錯誤9(Illegal hex string in BYTE statement)
             }
         }
-        else if(tmpCode->get_type()==4){//has OPcode
-            if(Opcode::is_in(tmpCode->get_opcode())){
-                tmpData = Opcode::get_objcode(tmpCode->get_opcode());
-                if(tmpCode->get_opcode()=="RSUB"){
-                    tmpCode->set_objcode(tmpData);
-                    if(tmpCode->get_data()!="")tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(23,Setting.language));
+        else if(tmpCode->get_type()==OPERATOR){                                     //如果是OPcode
+            if(Opcode::is_in(tmpCode->get_opcode())){                                   //且在optable找的到
+                tmpData = Opcode::get_objcode(tmpCode->get_opcode());                       //設定他的數值
+                if(tmpCode->get_opcode()=="RSUB"){                                          //如果是rsub
+                    if(tmpCode->get_data()!="")tmpCode->add_ErrMes(23,Setting.language);        //而且data有資料 噴錯誤23(Operand should not follow RSUB statement)
                 }
-                else{
-                    tmpStringData = tmpCode->get_data();
-                    if(tmpStringData.length()>=3){
-                        if((tmpStringData.at(tmpStringData.length()-1)=='X'||tmpStringData.at(tmpStringData.length()-1)=='x')&&tmpStringData.at(tmpStringData.length()-2)==',')tmpStringData = tmpStringData.substr(0,tmpStringData.length()-2);
+                else{                                                                       //其他部分
+                    tmpStr = tmpCode->get_data();                                               //獲取資料
+                    if(tmpStr.length()>=3){                                                     //如果字數>3
+                        if((tmpStr.at(tmpStr.length()-1)=='X'||tmpStr.at(tmpStr.length()-1)=='x')   //而且末兩碼是",x"
+                           &&tmpStr.at(tmpStr.length()-2)==','){
+                                tmpStr = tmpStr.substr(0,tmpStr.length()-2);                        //切出label
+                                tmpData+=0x8000;                                                    //objcode+0x8000
+                           }
                     }
-                    if(SymbolTAB->is_in(tmpStringData)){
-                        tmpData+=SymbolTAB->get_address(tmpStringData);
+                    if(SymbolTAB->is_in(tmpStr)){                                               //如果找的到label
+                        tmpData+=SymbolTAB->get_address(tmpStr);                                //加上去
                     }
-                    else tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(54,Setting.language));
-                    if(tmpCode->get_data().length()>=3){
-                        if(tmpCode->get_data().substr(tmpCode->get_data().length()-3,2)==",X")tmpData+=0x8000;
-                    }
-                    tmpCode->set_objcode(tmpData);
+                    else tmpCode->add_ErrMes(54,Setting.language);                              //找不到就噴錯誤54(Undefined symbol in operand field)
                 }
+                tmpCode->set_objcode(tmpData);                                              //把算出來的objcode存起來
             }
             else{
-                //cout<<tmpCode->toString()<<"has an error!!!\n";
-                tmpCode->add_ErrMes(ErrMes::get_ErrorMessage(2,Setting.language));
+                tmpCode->add_ErrMes(2,Setting.language);                                    //如果找不到operator 噴錯誤2(Illegal format in operation field)
             }
         }
-        else if(tmpCode->get_type()==5);    //Comment
+        else if(tmpCode->get_type()==COMMENT);                                              //Comment 不做事
     }
 
     //Pass3
